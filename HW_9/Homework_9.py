8cd03dfcf58e807d8952ae533166b676a1aca8d0
AlexeyChernyshkov <alchern1999@gmail.com>
2024-02-11 20:38:10 +0700

    Домашняя работа 9

--- HW_9/Homework_9.py
+++ HW_9/Homework_9.py
@@ -1,6 +1,7 @@
 #Пока что проект для диплома не выбрал.
 # Тест-кейсы будут для страницы https://makarova1507ana.github.io/to_do_list_test_example/
 
+# Пока просто набросок кейсов. Более подробное описание с шагами + автотесы сдам до 18.02
 #Ссылка на тесты: https://docs.google.com/spreadsheets/d/1qH_WlbfaGLw-Pnb8U2DdNistBdOlMPZ5RMr7R12uXzM/edit?usp=sharing
 
 #На данный момент автотесты не написаны
